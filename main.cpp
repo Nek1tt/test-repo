70328a92703d5bc0aca54175c3c1a250050b34c0
Nek1t_ <Mr.Nikita2912@gmail.com>
2025-03-08 16:56:25 +0700

    added 2.2 tests

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ TEST(JapaneseTest, body_contains) {
     EXPECT_TRUE(candle.body_contains(3.0));
 }
 
+TEST(JapaneseTest, contains) {
+    Candle candle{ 0.0, 3.0, 2.0, 3.0 };
+    EXPECT_TRUE(candle.contains(2.5));
+    EXPECT_FALSE(candle.contains(4.5));
+    EXPECT_TRUE(candle.contains(3.0));
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
